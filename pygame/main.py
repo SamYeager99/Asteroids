3b255c9f39d67583f0c8fcd0edbd79b789fc694d
SamYeager99 <samuelyeager99@gmail.com>
2024-09-06 18:03:01 -0500

    draw player

---
+++ pygame/main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black")
+        pygame.display.flip()
+
+        #limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
+
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
